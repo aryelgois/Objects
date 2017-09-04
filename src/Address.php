d71f30766dbc82019e5d2edd5e3933b0c38469bb
Aryel Mota Góis <aryel.gois@gmail.com>
2017-09-03 22:40:17 -0300

    Update Address object
    Now it uses aryelgois\utils\Database to handle database
    connections (you have to extend this class, because it
    is abstract)
    
    And it expects you to have a aryelgois\databases\Address
    configured in your server, to provide countries, states
    and counties. Other informations are set manually.
    
    This object is useful when you have a registry form and
    your clients provide their address. And the <select> for
    country, state and county should be filled dynamically.

--- src/Address.php
+++ src/Address.php
@@ -7,98 +7,133 @@
 
 namespace aryelgois\objects;
 
+use aryelgois\utils\Database;
+
 /**
- * A Person object defines someone in the real world
+ * An Address object to reference a place in the world
+ *
+ * It is built on top of aryelgois\databases\Address, which means it expects
+ * you have a database following that scheme in your server.
+ *
+ * @see https://www.github.com/aryelgois/databases
+ *
+ * Also, to access the database, this class uses aryelgois\utils\Database to
+ * handle the connection. As it is an abstract class, you have to extend it and
+ * provide your method for database errors, accordingly to your project needs.
+ *
+ * @see https://www.github.com/aryelgois/utils
+ *
+ * Even though, some fields aren't available in the database and you need to set
+ * them. These are for a more local information and should be provided by your
+ * clients.
  *
  * @author Aryel Mota Góis
  * @license MIT
  * @link https://www.github.com/aryelgois/objects
- * @version 0.1
+ * @version 0.2
  */
 class Address
 {
     /**
-     * Road, Avenue, these stuff
+     * Handles database interaction. It is used for getting the county, state
+     * and country.
      *
-     * @var string
+     * @var Database
      */
-    public $street;
+    protected $database;
     
     /**
-     * Buildings' number. An apartment's should go into $details
+     * Holds all loaded data about the address.
      *
-     * @var string
+     * @var mixed[]
      */
-    public $number;
-    
-    /**
-     * Here you put additional information
-     *
-     * @var string
-     */
-    public $detail;
-    
-    /**
-     * Implementation may vary from country to country
-     *
-     * @var string
-     */
-    public $zipcode;
-    
-    /**
-     * A county's small division
-     *
-     * @var string
-     */
-    public $neighborhood;
-    
-    /**
-     * It's the city
-     *
-     * @var string
-     */
-    public $county;
-    
-    /**
-     * Usually, a country is divided into states
-     *
-     * @var string
-     */
-    public $state;
-    
-    /**
-     * Up to now 195 countries exist
-     *
-     * @see http://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/
-     *
-     * @var string
-     */
-    public $country;
+    public $data = [];
     
     /**
      * Creates a new Address object
      *
-     * @param string $street       Do i need to repeat what is above?
-     * @param string $number       ..
-     * @param string $zipcode      ..
-     * @param string $neighborhood ..
-     * @param string $county       ..
-     * @param string $state        ..
-     * @param string $country      ..
-     * @param string $detail       Optional information
+     * @param Database $database An object to handle database connection and
+     *                           other stuffs. Make sure it is connected to the
+     *                           address database
      */
-    public function __construct($street, $number, $zipcode, $neighborhood, $county, $state, $country, $detail = '')
+    public function __construct(Database $database)
     {
-        /*
-         * Is there an easier way?
-         */
-        $this->street = $street;
-        $this->number = $number;
-        $this->detail = $detail;
-        $this->zipcode = $zipcode;
-        $this->neighborhood = $neighborhood;
-        $this->document = $county;
-        $this->state = $state;
-        $this->country = $country;
+        $this->database = $database;
+    }
+    
+    /**
+     * Retrieves some data from the Database
+     *
+     * It will ask for the county, state and country entries, based on the
+     * county index, because it's table is in the tip of the tables chain in the
+     * Database.
+     *
+     * @param integer $county_id The county index to fetch all the necessary
+     *                           data from the Database. You can know this from
+     *                           a registry form sent by your client.
+     *
+     * @return boolean For success of failure
+     */
+    public function loadData($county_id)
+    {
+        // fetch county
+        $query = "SELECT `state`, `name` FROM `counties` WHERE `id` = ?";
+        $county = Database::fetch($this->database->prepare($query, 'i', [$county_id]));
+        if (empty($county)) {
+            return false;
+        }
+        $county = $county[0];
+        
+        // fetch state
+        $query = "SELECT `country`, `code`, `name` FROM `states` WHERE `id` = ?";
+        $state = Database::fetch($this->database->prepare($query, 'i', [$county['state']]));
+        if (empty($state)) {
+            return false;
+        }
+        $state = $state[0];
+        
+        // fetch country
+        $query = "SELECT `code_a2`, `code_a3`, `code_number`, `name_en`, `name_local` FROM `countries` WHERE `id` = ?";
+        $country = Database::fetch($this->database->prepare($query, 'i', [$state['country']]));
+        if (empty($country)) {
+            return false;
+        }
+        $country = $country[0];
+        
+        // store data
+        $county['id'] = $county_id;
+        $state['id'] = $county['state'];
+        $country['id'] = $state['country'];
+        unset($county['state'], $state['country']);
+        $this->data['county'] = $county;
+        $this->data['state'] = $state;
+        $this->data['country'] = $country;
+        return true;
+    }
+    
+    /**
+     * Allows you to set the missing fields after calling getData()
+     *
+     * The keys in $data depends on your project. I recomend the following:
+     * - street:       Road, Avenue, these stuff
+     *
+     * - number:       Building numbers. should be an integer, but you can use a
+     *                 string like '13-A', or something to tell there is no
+     *                 number at all
+     *
+     * - neighborhood: A county's small division
+     *
+     * - zipcode:      Used by mailing to locate a place. It's implementation
+     *                 may vary from country to country
+     *
+     * - detail:       Here you put additional and complementary information
+     *
+     * @param mixed[] $data Its contents are merged into $this->data, so be
+     *                      careful not to replace the data from getData(),
+     *                      unless it is intentional.
+     */
+    public function setData($data)
+    {
+        $this->data = array_replace($this->data, $data);
     }
 }
